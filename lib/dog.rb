535f16b288d399843fcf69082036958acf22a36a
Denise Pendleton <dpendl1@hotmail.com>
2018-04-03 22:43:42 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -72,7 +72,7 @@ end
   end
 
   def self.create(dog_attr)
-    dog_attr = {name: @name, breed: @breed}
+    # dog_attr = {name: @name, breed: @breed}
     dog = Dog.new(dog_attr)
     # dog.save
     dog_attr.each {|key, value| self.send(("#{key}="), value)}
