646176a65da82b94ed78119c2c3984742f1e3817
Denise Pendleton <dpendl1@hotmail.com>
2018-04-03 23:47:55 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -59,7 +59,7 @@ def self.drop_table
   end
 
   def self.new_from_db(row)
-   new_dog = self.new(row[0], row[1], row[2])
+   new_dog = self.new(id: row[0], name: row[1], breed: row[2])
   new_dog
 end
 
