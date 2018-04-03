b5605f123190c5ff4d1375f4caf7e4836b333955
Denise Pendleton <dpendl1@hotmail.com>
2018-04-03 20:57:57 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -17,8 +17,15 @@ def self.create_table
     )
     SQL
     DB[:conn].execute(sql)
-
 end
+
+def self.drop_table
+    sql = <<-SQL
+      DROP TABLE IF EXISTS dogs
+    SQL
+
+    DB[:conn].execute(sql)
+  end
 
 
 end
