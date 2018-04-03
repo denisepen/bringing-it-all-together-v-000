94a000cc1af2011d348d0ff01da53eece0e0db03
Denise Pendleton <dpendl1@hotmail.com>
2018-04-03 22:57:38 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -81,7 +81,7 @@ end
     end.first
   end
 
-  
+
   def self.find_or_create_by(name:, breed:)
       dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, breed)
       if !dog.empty?
