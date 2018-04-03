a682d870b6ad59d911a254055bd07729d4af60dc
Denise Pendleton <dpendl1@hotmail.com>
2018-04-03 23:26:55 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -95,7 +95,7 @@ end
     SQL
 
     result = DB[:conn].execute(sql, id)[0]
-    dog = Dog.save(result[0], result[1], result[2])
+    dog = Dog.new(result[0], result[1], result[2])
     dog
   end
 
